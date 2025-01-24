4b9ee623ec9c4a826038ae6377e7d68c4f03945b
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-01-24 15:50:47 +0100

    triggering build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
+// typescript is giving me a headache, made this to trigger a build
+
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
