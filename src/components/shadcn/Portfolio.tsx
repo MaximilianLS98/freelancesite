b9d238dce990c19fd07a1d23bd10c492e56d49c7
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-01-24 11:45:50 +0100

    some weird import snuck its way into the code

--- src/components/shadcn/Portfolio.tsx
+++ src/components/shadcn/Portfolio.tsx
@@ -4,7 +4,6 @@ import React, { cache } from 'react'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
-import PreTextDebug from '../customComponents/PreTextDebug'
 
 const projects = [
   {
