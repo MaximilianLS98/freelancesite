be7e5f17992d37c5169a946201469626652383df
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2025-03-07 23:43:10 +0100

    Added another form of auth check, doesnt work

---
+++ src/access/apiaccess.ts
@@ -0,0 +1,16 @@
+import type { User } from '@/payload-types'
+import type { AccessArgs } from 'payload'
+
+type hasapiaccess = (args: AccessArgs<User>) => boolean
+
+export const apiaccess: hasapiaccess = ({ req: { user } }) => {
+    console.log("user in apiaccess", user);
+    if (!user) {
+        return false
+    }
+    // THis is not safe code, but using it for now. 
+    if (user.apiKey) {
+        return true
+    }
+    return false
+}
